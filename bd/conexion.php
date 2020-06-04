87b3cf73574b939f81d3e4d8710d708d187ebde2
MichaelBalladaresxd <michael.balladaresq@gmail.com>
2020-06-03 21:11:19 -0500

    ok

--- bd/conexion.php
+++ bd/conexion.php
@@ -7,7 +7,7 @@ function getPDO () {
     $pass = "";
 
     try {
-        $pdo = new PDO('mysql:host=162.211.86.2;dbname=foodsp_web', $user, $pass);
+        $pdo = new PDO('mysql:host=localhost;dbname=foodsp_web', $user, $pass);
         $pdo->exec("SET NAMES 'utf8';");
         return $pdo;
         
