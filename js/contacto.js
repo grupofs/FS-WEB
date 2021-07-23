636c3988230d95ca1112fd6f58126c66a6ac9c5d
Michael Balladares <michael.balladaresq@gmail.com>
2021-07-22 21:18:25 -0500

    ok final

--- js/contacto.js
+++ js/contacto.js
@@ -10,8 +10,13 @@ $(document).ready(function(){
             data:datos,
             success:function(response){
                 var res = JSON.parse(response); 
-                console.log('response', res.response)
                 if (res.response == 'ok') {
+                    Swal.fire(
+                        'Datos Enviados!',
+                        'Gracias por comunicarse con nosotros, en unos minutos nos pondremos en contacto con usted!',
+                        'success'
+                    );
+
                     $("#btnEnviar").text('Enviado!');
 
                     $(".form_name").val('');
